482f518a5f049a684a064fd63b7b9c09ec64f232
Nobuhiro_win <1ru.yamaguchi@gmail.com>
2022-01-03 10:52:15 +0900

    init

---
+++ onepassword/src/main/java/com/example/onepassword/OnepasswordApplication.java
@@ -0,0 +1,13 @@
+package com.example.onepassword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnepasswordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnepasswordApplication.class, args);
+	}
+
+}
