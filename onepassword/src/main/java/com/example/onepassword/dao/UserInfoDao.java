8820363164c9b9833ea966574aeebdfcf254022b
Nobuhiro_win <1ru.yamaguchi@gmail.com>
2022-02-06 14:47:56 +0900

    パッケージ構成変更 mapper追加

--- onepassword/src/main/java/com/example/onepassword/dao/UserInfoDao.java
+++ onepassword/src/main/java/com/example/onepassword/dao/UserInfoDao.java
@@ -2,14 +2,24 @@ package com.example.onepassword.dao;
 
 import com.example.onepassword.dto.LoginDto;
 import com.example.onepassword.entity.UserInfo;
+import com.example.onepassword.mapper.UserInfoMapper;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 /**
- * user_infoテーブルに関するmapper
+ * user_infoテーブルのdaoクラス
  * 
  */
-public interface UserInfoDao {
+@Component
+public class UserInfoDao {
+
+    @Autowired
+    private UserInfoMapper userInfoMapper;
 
     /** LoginDtoに一致するユーザ情報を取得する */
-    public UserInfo selectUserInfo(LoginDto loginDto);
+    public UserInfo selectUserInfo(LoginDto loginDto) {
+        return userInfoMapper.selectUserInfo(loginDto);
+    }
 
 }
